bf39c252321693291d12660e08ca904bc6b11b8e
seojiwon <sjs3326@naver.com>
2023-11-16 17:34:44 +0900

    feat: header, footer 추가

---
+++ src/components/layout/item/NavItem.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  { value: "spot", name: "여행지" },
+  { value: "plan", name: "플래너" },
+  { value: "board", name: "게시판" },
+  { value: "notice", name: "공지사항" },
+  { value: "qna", name: "QnA" },
+  { value: "login", name: "로그인" },
+  { value: "join", name: "회원가입" },
+]);
+</script>
+
+<template>
+  <div class="nav mt-6 elevation-2 rounded mb-10">
+    <v-row>
+      <v-col class="d-flex justify-space-between">
+        <div>
+          <v-btn
+            class="font-weight-black"
+            variant="plain"
+            rounded="lg"
+            v-for="i in 5"
+            >{{ items[i - 1].name }}</v-btn
+          >
+        </div>
+        <div>
+          <v-btn
+            class="font-weight-black"
+            variant="plain"
+            rounded="lg"
+            v-for="i in 2"
+            >{{ items[4 + i].name }}</v-btn
+          >
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+  <!-- <v-divider class="mt-3 mb-10 border-opacity-25"></v-divider> -->
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,12 +4,52 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+// Vuetify
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import * as styles from "vuetify/styles";
+
+// vurify icon
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import {
+  mdiAccount,
+  mdiLogout,
+  mdiFacebook,
+  mdiTwitter,
+  mdiInstagram,
+  mdiLinkedin,
+  mdiGithub,
+} from "@mdi/js";
+
+const vuetify = createVuetify({
+  components,
+  icons: {
+    defaultSet: "mdi",
+    aliases: {
+      ...aliases,
+      account: mdiAccount,
+      logout: mdiLogout,
+      facebook: mdiFacebook,
+      twitter: mdiTwitter,
+      instagram: mdiInstagram,
+      linkedin: mdiLinkedin,
+      github: mdiGithub,
+    },
+    sets: {
+      mdi,
+    },
+  },
+  directives,
+  styles,
+});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+// app.mount("#app");
+app.use(vuetify).mount("#app");
