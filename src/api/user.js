df0543a28beb718065ef87931c21176d6b96474a
wjddlsee <wjddlsee@gmail.com>
2023-11-21 14:48:32 +0900

    feat: 비밀번호 변경 구현

--- src/api/user.js
+++ src/api/user.js
@@ -1,10 +1,11 @@
-import { localAxios } from "../util/http-commons";
+import { localAxios, simpleAxios } from "../util/http-commons";
 import { useCookies } from "vue3-cookies";
 import { useMemberStore } from "../stores/member";
 
 const { cookies } = useCookies();
 
 const local = localAxios();
+const simple = simpleAxios();
 
 async function findById(userId, success, fail) {
   const memberStore = useMemberStore();
@@ -16,4 +17,22 @@ async function findById(userId, success, fail) {
   console.log("success findById");
 }
 
-export { findById };
+async function sendEmail(email, success, fail) {
+  console.log('email', email);
+  simple.post(`/mail/password`, JSON.stringify(email)).then(success).catch(fail);
+}
+
+async function changePassword(param, success, fail) {
+  // param에서 토큰, 비밀번호 분리해서 헤더, DTO 따로 전달
+  console.log('param?', param);
+  const password = param.password;
+  const accessToken = param.accessToken;
+  await local.patch(`/users/password`, JSON.stringify(password), {
+    headers: {
+      Authorization: accessToken,
+    },
+  }).then(success).catch(fail);
+
+}
+
+export { findById, sendEmail, changePassword };
